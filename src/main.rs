f4806173dc26e445a8160d5b8d3523852e600337
scullionw <williamscullion@gmail.com>
2019-09-23 01:54:09 -0400

    no window

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,21 @@
 
 use rust_embed::RustEmbed;
 use std::env;
-use std::error::Error;
 use std::fs;
+use std::os::windows::process::CommandExt;
 use std::path::Path;
 use std::process::Command;
+use tempfile::TempDir;
+
+const CREATE_NO_WINDOW: u32 = 0x08000000;
 
 #[derive(RustEmbed)]
 #[folder = "dist/"]
 struct Asset;
 
 fn main() {
-    let temp_dir = Path::new("temp");
-
-    if temp_dir.exists() {
-        fs::remove_dir_all(temp_dir).expect("Could not remove temp directory");
-    }
-
-    fs::create_dir(temp_dir).expect("Could not create temp directory");
+    let temp = TempDir::new().expect("Could not create temp dir");
+    let temp_dir = temp.path();
 
     let mut exe_path = None;
     for file in Asset::iter() {
@@ -44,28 +42,16 @@ fn main() {
     }
 
     if let Some(exe_path) = exe_path {
-        let current_dir = env::current_dir().expect("Could not get current directory");
-        reactor(temp_dir, &exe_path).expect("Reactor execution error");
-        env::set_current_dir(current_dir).expect("Could not switch back to original directory");
-        fs::remove_dir_all(temp_dir).expect("Could not remove temp directory on shutdown");
+        execute(temp_dir, &exe_path);
     } else {
         eprintln!("No executable found!");
     }
 }
 
-fn reactor(temp_dir: &Path, exe_path: &Path) -> Result<(), Box<dyn Error>> {
+fn execute(temp_dir: &Path, exe_path: &Path) {
     env::set_current_dir(temp_dir).expect("Could not change directory");
-    let output = Command::new(exe_path).output()?;
-
-    if output.status.success() {
-        println!("status: {}", output.status);
-        println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-        eprintln!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-        Ok(())
-    } else {
-        println!("status: {}", output.status);
-        println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-        eprintln!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-        Err("reactor command failed.".into())
-    }
+    let mut cmd = Command::new(exe_path);
+    cmd.creation_flags(CREATE_NO_WINDOW);
+    let mut child = cmd.spawn().expect("Could not spawn command");
+    child.wait().expect("command wasn't running");
 }
